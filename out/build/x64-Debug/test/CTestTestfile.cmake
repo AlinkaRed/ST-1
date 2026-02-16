33ea270ec59fe05ad0cf354cf83d1be1910736e8
Alina <red.alinka.malinka@gmail.com>
2026-02-16 12:27:05 +0300

    first

---
+++ out/build/x64-Debug/test/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/USER/ST-1/test
+# Build directory: C:/Users/USER/ST-1/out/build/x64-Debug/test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../_deps/googletest-build")
